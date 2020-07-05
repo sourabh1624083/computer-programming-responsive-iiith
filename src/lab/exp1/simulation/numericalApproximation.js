62b0917fb27b4e6ffb4cfeffb2b9ec6ce26881ba
sourabh1624083 <sourabh1624083@gmail.com>
2020-07-06 02:13:31 +0530

    W3D1 new Changes src/lab/exp3/simulation/addControlFlow.js

--- src/lab/exp1/simulation/numericalApproximation.js
+++ src/lab/exp1/simulation/numericalApproximation.js
@@ -288,7 +288,7 @@ window.view = {
                 alert('Enter numeric value of a and b');
                 return false;
             } else if (valueA2 > 10 || valueA2 < 0 || valueB2 > 10 || valueB2 < 0) {
-                alert('Value Should be between 1 to 10')
+                alert('Invalid Input')
                 return false;
             } else if (valueA2 >= valueB2 || valueB2 > 30) {
                 alert('Integration Limits are from 0 to 30, b > a and b-a >= 1');
